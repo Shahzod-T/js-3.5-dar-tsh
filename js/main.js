97940e73b084bc1a0ccac7476352542de0138c9a
Shahzod-T <shahzodbek299@gmail.com>
2022-02-19 14:06:53 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+alert("Assalomualaykum Xurmatli Inson   Bu Yera Siz faqat Bootstrpga Oid Savolarga Javob Berasiz Sizga Omad Tilaymiz");
+
+var firstname = prompt(" Siznig ismingz?");
+var Bootstrap = prompt("Bootstrap ni online ulash uchun nima qilish kerak?");
+var Pagination = prompt("Pagination haqida qisqa?");
+var Carusel = prompt("Carusel xaqida qisqa?");
+var Row = prompt("Row xaqida qisqa?");
+var Form = prompt("Nechta Form classlarini blasiz?");
+var Select = prompt("Select xaqida qisqa?");
+var input = prompt("Input xaqida qisqa?");
+var Label = prompt("Label xaqida qisqa?");
+var checkandraido = prompt("Check va Raido Farqi qisqa?");
+var Range = prompt("Range xaqida qisqa?");
+var inputgroup = prompt("Input-group spanlari bilan qisqa?");
+var Margin = prompt("Margin xaqida qisqa?");
+var Padding = prompt("Padding xaqida qisqa?");
+
+
+
+
+console.log(firstname +"\n" + Bootstrap +"\n" + Pagination +"\n" + Carusel +"\n" + Row +"\n" + Form +"\n" + Select +"\n" + input +"\n" + 
+
+Label +"\n" + checkandraido +"\n" + Range +"\n" + inputgroup + "\n" + Margin + "\n" + Padding
+
+);
